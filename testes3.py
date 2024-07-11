22ef7fe2687fca557808e8a9107e35e4d9d8c1a5
João Sena <126720426+jS3na@users.noreply.github.com>
2024-07-11 10:39:14 -0300

    Update testes3.py

--- testes3.py
+++ testes3.py
@@ -4,4 +4,6 @@ semPagar = pg.locateOnScreen(r'CobraOi\imagens\baixarfatura.png', region=(970, 2
 
 x,y=pg.center(semPagar)
 pg.moveTo(x,y)
+
+https://oilogin360.oi.net.br/nidp/idff/sso?id=OiPasswordClassCorporativoId&sid=0&option=credential&sid=0&target=https%3A%2F%2Foi360.oi.net.br%2Fprweb%2FPRServletCustom%2FAX6P2laLe91D09R0jTjfNJdv0u0s3qcA*%2F%21STANDARD%3FpyActivity%3DData-Portal.ShowDesktop#
     
