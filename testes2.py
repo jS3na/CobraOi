5aa431460733c43a202be48906bfa9b82d5ed335
João Sena <passosjoaogabriel29@gmail.com>
2025-07-16 22:22:49 -0300

    Delete testes2.py

--- testes2.py
+++
@@ -1,6 +0,0 @@
-import pyautogui as pg
-
-btt = pg.locateOnScreen(r'.\imagens\doc.png', region=(290, 400, 520, 500), confidence=0.7) #BOTÃO DE DOCUMENTOS, PARA ENVIAR A FATURA
-x, y = pg.center(btt)
-pg.click(x, y, duration=0.5)
-
