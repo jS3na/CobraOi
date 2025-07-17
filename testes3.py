f5d77c6e3bff6d9f3b34634da6ff9808c89a9e40
João Sena <passosjoaogabriel29@gmail.com>
2025-07-16 22:22:56 -0300

    Delete testes3.py

--- testes3.py
+++
@@ -1,6 +0,0 @@
-import pyautogui as pg
-
-semPagar = pg.locateOnScreen(r'CobraOi\imagens\baixarfatura.png', region=(970, 250, 250, 600), confidence = 0.75)
-
-x,y=pg.center(semPagar)
-pg.moveTo(x,y)
